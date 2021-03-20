cdbc7672cee3f1430b38bf065f59cb8fedd9d4ba
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-03-20 13:57:11 +0100

    [VARIOS] Cambio en los mapping de los Controller.

--- backend/src/main/java/es/udc/paproject/backend/rest/controllers/InfoSearchController.java
+++ backend/src/main/java/es/udc/paproject/backend/rest/controllers/InfoSearchController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/catalog")
+@RequestMapping("/search")
 public class InfoSearchController {
 
     @Autowired
