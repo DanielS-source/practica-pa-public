09fbf14e448991447ba231dcaec55ec7eb68db09
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-03-21 15:00:42 +0100

    [BUS_PRU] Conversor de pruebas a su resumen.

--- backend/src/main/java/es/udc/paproject/backend/rest/dtos/SportTestConversor.java
+++ backend/src/main/java/es/udc/paproject/backend/rest/dtos/SportTestConversor.java
@@ -1,4 +1,30 @@
 package es.udc.paproject.backend.rest.dtos;
 
+import es.udc.paproject.backend.model.entities.SportTest;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.temporal.ChronoUnit;
+
 public class SportTestConversor {
+
+    private SportTestConversor() {}
+
+    public final static SportTestSummaryDto toSportTestSummaryDto(SportTest sportTest) {
+
+        return new SportTestSummaryDto(
+                sportTest.getId(),
+                sportTest.getName(),
+                sportTest.getSportTestType().getId(),
+                sportTest.getProvince().getId(),
+                toMillis(sportTest.getTestStart()),
+                sportTest.getAverageRating(),
+                sportTest.getTimesRated()
+        );
+
+    }
+
+    private final static long toMillis(LocalDateTime date) {
+        return date.truncatedTo(ChronoUnit.MINUTES).atZone(ZoneOffset.systemDefault()).toInstant().toEpochMilli();
+    }
 }
