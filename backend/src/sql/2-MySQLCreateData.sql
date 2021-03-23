461fc09a46192acfab03398966866a991be11cc7
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-03-23 20:11:09 +0100

    [VARIOS] FIX fechas sentencias SQL.

--- backend/src/sql/2-MySQLCreateData.sql
+++ backend/src/sql/2-MySQLCreateData.sql
@@ -10,16 +10,16 @@ INSERT INTO SportTestType (name) VALUES ('Running');
 INSERT INTO SportTestType (name) VALUES ('Ciclismo');
 
 INSERT INTO SportTest (name, description, testStart, price, maxParticipants, location, provinceId, sportTestTypeId, participants, timesRated, averageRating, version)
-    VALUES ('Prueba 1', 'DescripcionP1', '01-07-2020 16:00:00', 6.99, 500, 'Micasa', 1, 1, 0, 0, 0, 1);
+    VALUES ('Prueba 1', 'DescripcionP1', '2020-07-01 16:00:00', 6.99, 500, 'Micasa', 1, 1, 0, 0, 0, 1);
 
 INSERT INTO SportTest (name, description, testStart, price, maxParticipants, location, provinceId, sportTestTypeId, participants, timesRated, averageRating, version)
-    VALUES ('Prueba 2', 'DescripcionP2', '01-01-2020 16:00:00', 6.99, 100, 'Micasa', 2, 1, 0, 0, 0, 1);
+    VALUES ('Prueba 2', 'DescripcionP2', '2020-01-01 16:00:00', 6.99, 100, 'Micasa', 2, 1, 0, 0, 0, 1);
 
 INSERT INTO SportTest (name, description, testStart, price, maxParticipants, location, provinceId, sportTestTypeId, participants, timesRated, averageRating, version)
-    VALUES ('Prueba 3', 'DescripcionP3', '01-02-2020 16:00:00', 6.99, 300, 'Micasa', 1, 2, 0, 0, 0, 1);
+    VALUES ('Prueba 3', 'DescripcionP3', '2020-02-01 16:00:00', 6.99, 300, 'Micasa', 1, 2, 0, 0, 0, 1);
 
 INSERT INTO SportTest (name, description, testStart, price, maxParticipants, location, provinceId, sportTestTypeId, participants, timesRated, averageRating, version)
-    VALUES ('Prueba 4', 'DescripcionP4', '01-03-2020 16:00:00', 6.99, 2,   'Micasa', 2, 2, 0, 0, 0, 1);
+    VALUES ('Prueba 4', 'DescripcionP4', '2020-03-01 16:00:00', 6.99, 2,   'Micasa', 2, 2, 0, 0, 0, 1);
 
 INSERT INTO User (userName, password, firstName, lastName, email, role)
     VALUES ('competitor1', 'ficrunner', 'firstName', 'lastName', 'email@email.email', 1);
