a630699f31c86459c73ca13fa08ea9981181b2f6
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-03-27 20:26:07 +0100

    [VARIOS] Fix create data de la BD.

--- backend/src/sql/2-MySQLCreateData.sql
+++ backend/src/sql/2-MySQLCreateData.sql
@@ -13,13 +13,13 @@ INSERT INTO SportTest (name, description, testStart, price, maxParticipants, loc
     VALUES ('Prueba 1', 'DescripcionP1', '2020-07-01 16:00:00', 6.99, 500, 'Micasa', 1, 1, 0, 0, 0, 1);
 
 INSERT INTO SportTest (name, description, testStart, price, maxParticipants, location, provinceId, sportTestTypeId, participants, timesRated, averageRating, version)
-    VALUES ('Prueba 2', 'DescripcionP2', '2020-01-01 16:00:00', 6.99, 100, 'Micasa', 2, 1, 0, 0, 0, 1);
+    VALUES ('Prueba 2', 'DescripcionP2', '2022-01-01 16:00:00', 6.99, 100, 'Micasa', 2, 1, 0, 0, 0, 1);
 
 INSERT INTO SportTest (name, description, testStart, price, maxParticipants, location, provinceId, sportTestTypeId, participants, timesRated, averageRating, version)
-    VALUES ('Prueba 3', 'DescripcionP3', '2020-02-01 16:00:00', 6.99, 300, 'Micasa', 1, 2, 0, 0, 0, 1);
+    VALUES ('Prueba 3', 'DescripcionP3', '2022-02-01 16:00:00', 6.99, 300, 'Micasa', 1, 2, 0, 0, 0, 1);
 
 INSERT INTO SportTest (name, description, testStart, price, maxParticipants, location, provinceId, sportTestTypeId, participants, timesRated, averageRating, version)
-    VALUES ('Prueba 4', 'DescripcionP4', '2020-03-01 16:00:00', 6.99, 2,   'Micasa', 2, 2, 0, 0, 0, 1);
+    VALUES ('Prueba 4', 'DescripcionP4', '2022-03-01 16:00:00', 6.99, 2,   'Micasa', 2, 2, 0, 0, 0, 1);
 
 INSERT INTO User (userName, password, firstName, lastName, email, role)
     VALUES ('competitor1', '$2a$10$uicNGMO41KcyofyMPaCCwuQ89o1WM/wn5f9TGgCuFjZrfDLnECPyi', 'firstName', 'lastName', 'email@email.email', 0);
