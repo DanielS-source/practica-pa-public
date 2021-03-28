067993c90e72eb7df41aa9f5f6bfa6d520768c87
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-03-28 11:43:17 +0200

    [BUS-PRU] Cambio implementacion DAO de find.

--- backend/src/main/java/es/udc/paproject/backend/model/entities/SportTestDao.java
+++ backend/src/main/java/es/udc/paproject/backend/model/entities/SportTestDao.java
@@ -1,6 +1,19 @@
 package es.udc.paproject.backend.model.entities;
 
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface SportTestDao extends PagingAndSortingRepository<SportTest,Long>, CustomizedSportTestDao {
+import java.time.LocalDate;
+
+public interface SportTestDao extends PagingAndSortingRepository<SportTest,Long> {
+
+    @Query("SELECT st FROM SportTest st WHERE " +
+            "(?1 IS NULL OR st.province.id = ?1) " +
+            "AND (?2 IS NULL OR st.sportTestType.id = ?2) " +
+            "AND (?3 IS NULL OR st.testStart >= ?3)" +
+            "AND (?4 IS NULL OR st.testStart <= ?4)" +
+            "ORDER BY st.testStart DESC")
+    Slice<SportTest> find(Long provinceId, Long testTypeId, LocalDate startDate, LocalDate endDate, int page, int size);
+
 }
