cd1a16274beb153b26decf133192bbec06396816
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-04-27 16:16:51 +0200

    [BUS-PRU] Terminado FindSportingEvents.js.

--- frontend/src/modules/eventsearch/Components/FindSportingEvents.js
+++ frontend/src/modules/eventsearch/Components/FindSportingEvents.js
@@ -32,10 +32,10 @@ const FindSportingEvents = () => {
         <form className="form-inline mt-2 mt-md-0" onSubmit={e => handleSubmit(e)}>
 
             <input id="startDate" type="text" className="form-control mr-sm-2"
-                   value={startDate} onChange={e => setKeywords(e.target.value)}/>
+                   value={startDate} onChange={e => setStartDate(e.target.value)}/>
 
             <input id="endDate" type="text" className="form-control mr-sm-2"
-                   value={endDate} onChange={e => setKeywords(e.target.value)}/>
+                   value={endDate} onChange={e => setEndDate(e.target.value)}/>
 
             <button type="submit" className="btn btn-primary my-2 my-sm-0">
                 <FormattedMessage id='project.global.buttons.search'/>
