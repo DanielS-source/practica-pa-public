e11cd1d6715cfbebde3b392073fa373a10b4babb
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-04-27 15:58:51 +0200

    [BUS-PRU] Index de eventsearch.

---
+++ frontend/src/modules/eventsearch/index.js
@@ -0,0 +1,9 @@
+import * as actions from './actions';
+import * as actionTypes from './actionTypes';
+import reducer from './reducer';
+import * as selectors from './selectors';
+
+export {default as FindSportingEvents} from './components/FindSportingEvents';
+export {default as FindSportingEventsResult} from './components/FindSportingEventsResult';
+
+export default {actions, actionTypes, reducer, selectors};
