105510b9d6d4516f73111b35b85b63d34b80a3a0
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-04-27 21:55:47 +0200

    [BUS-PRU] Home hace las cosas ahora.

--- frontend/src/modules/app/components/Home.js
+++ frontend/src/modules/app/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
+import {FindSportingEvents, FindSportingEventsResult} from "../../eventsearch";
 
 const Home = () => (
     <div className="text-center">
         <FormattedMessage id="project.app.Home.welcome"/>
+        <FindSportingEventsResult/>
     </div>
 );
 
