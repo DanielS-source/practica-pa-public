2ec0cc84894cc40cbd6ed9ece918b181b334486b
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-04-27 16:13:41 +0200

    [BUS-PRU] SportingEventLink.js.

--- frontend/src/modules/eventsearch/Components/FindSportingEvents.js
+++ frontend/src/modules/eventsearch/Components/FindSportingEvents.js
@@ -9,15 +9,42 @@ const FindSportingEvents = () => {
 
     const dispatch = useDispatch();
     const history = useHistory();
-    const [categoryId, setCategoryId] = useState('');
-    const [keywords, setKeywords] = useState('');
+    const [provinceId, setProvinceId] = useState('');
+    const [sportingEventTypeId, setSportingEventTypeId] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
 
     const handleSubmit = event => {
         event.preventDefault();
         dispatch(actions.findSportingEvents(
-            {categoryId: toNumber(categoryId),
-                keywords: keywords.trim(), page: 0}));
-        history.push('/catalog/find-products-result');
+            {provinceId: toNumber(provinceId),
+                sportingEventTypeId: toNumber(sportingEventTypeId),
+                startDate: startDate.trim(),
+                endDate: endDate.trim(),
+                page: 0}));
+        history.push('/search/find-sporting-events-result');
     }
 
+    const toNumber = value => value.length > 0 ? Number(value) : null;
+
+    return (
+
+        <form className="form-inline mt-2 mt-md-0" onSubmit={e => handleSubmit(e)}>
+
+            <input id="startDate" type="text" className="form-control mr-sm-2"
+                   value={startDate} onChange={e => setKeywords(e.target.value)}/>
+
+            <input id="endDate" type="text" className="form-control mr-sm-2"
+                   value={endDate} onChange={e => setKeywords(e.target.value)}/>
+
+            <button type="submit" className="btn btn-primary my-2 my-sm-0">
+                <FormattedMessage id='project.global.buttons.search'/>
+            </button>
+
+        </form>
+
+    );
+
 }
+
+export default FindSportingEvents;
