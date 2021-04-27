0c683f79a00a0d1be15fb5b8cbd09a3ecbb7fa8c
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-04-27 15:56:25 +0200

    [BUS-PRU] Reducer de eventsearch.

--- frontend/src/modules/eventsearch/actionTypes.js
+++ frontend/src/modules/eventsearch/actionTypes.js
@@ -1,1 +1,2 @@
-export const CLEAR_SPORTING_EVENTS_SEARCH = "project/catalog/clearSportingEventSearch";
+export const CLEAR_SPORTING_EVENTS_SEARCH = "project/search/clearSportingEventSearch";
+export const FIND_SPORTING_EVENTS_COMPLETED = "project/search/clearSportingEventSearch";
