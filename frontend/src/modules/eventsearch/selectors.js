cbbb4398e194a6e293a31a30068cd5da92c14920
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-05-01 15:42:13 +0200

    [BUS-PRU] Selectores con get name.

--- frontend/src/modules/eventsearch/selectors.js
+++ frontend/src/modules/eventsearch/selectors.js
@@ -6,5 +6,37 @@ export const getSportingEventSearch = state =>
 export const getSportingEventTypes = state =>
     getModuleState(state).sportingEventSearch;
 
+export const getSportingEventTypeName = (sportingEventTypes, id) => {
+
+    if (!sportingEventTypes) {
+        return '';
+    }
+
+    const sportingEventType = sportingEventTypes.find(sportingEventType => sportingEventType.id === id);
+
+    if (!sportingEventTypes) {
+        return '';
+    }
+
+    return sportingEventType.name;
+
+}
+
 export const getProvinces = state =>
     getModuleState(state).sportingEventSearch;
+
+export const getProvinceName = (provinces, id) => {
+
+    if (!provinces) {
+        return '';
+    }
+
+    const province = provinces.find(province => province.id === id);
+
+    if (!provinces) {
+        return '';
+    }
+
+    return province.name;
+
+}
