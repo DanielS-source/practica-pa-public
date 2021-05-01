f8403d3b91fd265d588917d006961fe44711f3e9
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-05-01 16:03:18 +0200

    [BUS-PRU] Reductores  de los selectores.

--- frontend/src/modules/eventsearch/actionTypes.js
+++ frontend/src/modules/eventsearch/actionTypes.js
@@ -1,2 +1,4 @@
 export const CLEAR_SPORTING_EVENTS_SEARCH = "project/search/clearSportingEventSearch";
 export const FIND_SPORTING_EVENTS_COMPLETED = "project/search/findSportingEventsCompleted";
+export const FIND_ALL_SPORTING_EVENTS_TYPES_COMPLETED = "project/";
+export const FIND_ALL_PROVINCES_COMPLETED = "";
