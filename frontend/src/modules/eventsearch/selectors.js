26ffdd16c10f9e7fe75d95d3cdea653b84855b3a
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-05-01 15:33:08 +0200

    [BUS-PRU] Selectores hechos.

--- frontend/src/modules/eventsearch/selectors.js
+++ frontend/src/modules/eventsearch/selectors.js
@@ -2,3 +2,9 @@ const getModuleState = state => state.search;
 
 export const getSportingEventSearch = state =>
     getModuleState(state).sportingEventSearch;
+
+export const getSportingEventTypes = state =>
+    getModuleState(state).sportingEventSearch;
+
+export const getProvinces = state =>
+    getModuleState(state).sportingEventSearch;
