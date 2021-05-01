38f89de565bcdbed10880065770d3abd403e49c4
Xoán Vázquez González <xoan.vazquez@udc.es>
2021-05-01 16:19:21 +0200

    [BUS-PRO] Cambiado index.js

--- frontend/src/modules/eventsearch/index.js
+++ frontend/src/modules/eventsearch/index.js
@@ -5,5 +5,5 @@ import * as selectors from './selectors';
 
 export {default as FindSportingEvents} from './components/FindSportingEvents';
 export {default as FindSportingEventsResult} from './components/FindSportingEventsResult';
-
+export {default as SportingEventDetails} from './components/SportingEventDetails';
 export default {actions, actionTypes, reducer, selectors};
