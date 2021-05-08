c301c7b839fa8146839b7f2501527bd0c6a98702
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-05-08 20:18:04 +0200

    [INS-PRU] DeliverDorsalForm mas o menos

--- frontend/src/modules/registrations/components/DeliverDorsalForm.js
+++ frontend/src/modules/registrations/components/DeliverDorsalForm.js
@@ -1,14 +1,75 @@
-import React from 'react';
-import {useSelector} from 'react-redux';
+import React, {useState} from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 import {FormattedMessage} from 'react-intl';
+import {useHistory} from 'react-router-dom';
 
+import {Errors} from '../../common';
+import * as actions from '../actions';
 import * as selectors from '../selectors';
 
 const DeliverDorsalForm = ({SportingEventId}) => {
 
     const dorsal = useSelector(selectors.getDorsal());
+    const [creditCard, setCreditCard] = useState('');
+    const [inscriptionId, setInscriptionId] = useState('');
+    const [backendErrors, setBackendErrors] = useState(null);
+    let form;
 
+    const handleSubmit = event => {
 
+        event.preventDefault();
+
+        if (form.checkValidity()) {
+
+            dispatch(actions.deliverDorsal(inscriptionId, SportingEventId, creditCard.trim(),
+                () => history.push('/registrations/inscription-completed'),
+                errors => setBackendErrors(errors)));
+
+        } else {
+            setBackendErrors(null);
+            form.classList.add('was-validated');
+        }
+    }
+
+    return (
+        <div>
+            <Errors errors={backendErrors}
+                    onClose={() => setBackendErrors(null)}/>
+            <div className="card bg-light border-dark">
+                <h5 className="card-header">
+                    <FormattedMessage id="project.registrations.regForm.title"/>
+                </h5>
+                <div className="card-body">
+                    <form ref={node => form = node}
+                          className="needs-validation" noValidate
+                          onSubmit={(e) => handleSubmit(e)}>
+                        <div className="form-group row">
+                            <label htmlFor="creditCard" className="col-md-3 col-form-label">
+                                <FormattedMessage id="project.global.fields.creditCard"/>
+                            </label>
+                            <div className="col-md-4">
+                                <input type="text" id="creditCard" className="form-control"
+                                       value={creditCard}
+                                       onChange={e => setCreditCard(e.target.value)}
+                                       autoFocus
+                                       required/>
+                                <div className="invalid-feedback">
+                                    <FormattedMessage id='project.global.validator.required'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <div className="offset-md-3 col-md-1">
+                                <button type="submit" className="btn btn-primary">
+                                    <FormattedMessage id="project.global.buttons.inscribe"/>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 
 }
 
