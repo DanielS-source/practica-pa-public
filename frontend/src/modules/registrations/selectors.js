3b16242c1f842a645520beee8ebddfd0e6a2a61a
Xoán Vázquez González <xoan.vazquez@udc.es>
2021-05-08 17:56:48 +0200

    [INS-PRO] Merge raro

--- frontend/src/modules/registrations/selectors.js
+++ frontend/src/modules/registrations/selectors.js
@@ -1,4 +1,4 @@
-const getModuleState = state => state.registrations;
+const getModuleState = state => state.registration;
 
 export const getLastRegistrationId = state =>
     getModuleState(state).getLastInscriptionId;
