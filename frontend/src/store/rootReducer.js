506c9d101721ae27c3cbb8fcda04d9104249c4be
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-05-08 17:57:34 +0200

    [INS-PRU] FIX root reducer.

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -3,13 +3,13 @@ import {combineReducers} from 'redux';
 import app from '../modules/app';
 import users from '../modules/users';
 import eventSearch from '../modules/eventsearch';
-import registration from '../modules/registration';
+import registrations from '../modules/registrations';
 
 const rootReducer = combineReducers({
     app: app.reducer,
     users: users.reducer,
     eventSearch: eventSearch.reducer,
-    registration: registration.reducer
+    registrations: registrations.reducer
 });
 
 export default rootReducer;
