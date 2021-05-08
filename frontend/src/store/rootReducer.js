789b4677b9e35b6a5b63070dd73ceaf1640926a9
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-05-08 17:24:01 +0200

    [INS-PRU] Añadido reducer al root reducer.

--- frontend/src/store/rootReducer.js
+++ frontend/src/store/rootReducer.js
@@ -3,11 +3,13 @@ import {combineReducers} from 'redux';
 import app from '../modules/app';
 import users from '../modules/users';
 import eventSearch from '../modules/eventsearch';
+import registration from '../modules/registration';
 
 const rootReducer = combineReducers({
     app: app.reducer,
     users: users.reducer,
-    eventSearch: eventSearch.reducer
+    eventSearch: eventSearch.reducer,
+    registration: registration.reducer
 });
 
 export default rootReducer;
