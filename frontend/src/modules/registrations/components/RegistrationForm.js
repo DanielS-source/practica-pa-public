bdf5e0f660e0551323c6792a7bd5339a122e44b8
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-05-11 13:35:12 +0200

    [ENT-DOR] Quitados imports

--- frontend/src/modules/registrations/components/RegistrationForm.js
+++ frontend/src/modules/registrations/components/RegistrationForm.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {useSelector, useDispatch} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {FormattedMessage} from 'react-intl';
 import {useHistory} from 'react-router-dom';
 
