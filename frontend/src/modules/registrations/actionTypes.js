c1a5f20858f16d1b604f7f053ac360621a56c593
DanielS-source <72018929+DanielS-source@users.noreply.github.com>
2021-05-15 17:30:06 +0200

    [ENT-DOR] Borrado un actionType

--- frontend/src/modules/registrations/actionTypes.js
+++ frontend/src/modules/registrations/actionTypes.js
@@ -1,4 +1,3 @@
 export const INSCRIPTION_COMPLETED = "project/registrations/inscription-completed";
-export const DORSAL_DELIVERED = "project/register/dorsalDelivered";
 export const CLEAR_REGISTRATION_SEARCH = "project/register/dorsalDelivered";
 export const FIND_REGISTRATION_COMPLETED = "project/register/dorsalDelivered";
