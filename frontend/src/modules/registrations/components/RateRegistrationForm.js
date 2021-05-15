1a9f68975d3cc60a64cb655a572c4d5185ae700a
Xoán Vázquez González <xoan.vazquez@udc.es>
2021-05-15 18:52:22 +0200

    [PUN-PRU] Hecho el form (no funcional todavia)

--- frontend/src/modules/registrations/components/RateRegistrationForm.js
+++ frontend/src/modules/registrations/components/RateRegistrationForm.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import * as actions from "../actions";
 import {useDispatch, useSelector} from "react-redux";
+import {Errors} from "../../common";
+import {FormattedMessage} from "react-intl";
 
 const RateRegistrationForm = ({id}) => {
 
@@ -16,7 +18,7 @@ const RateRegistrationForm = ({id}) => {
         if (form.checkValidity()) {
 
             dispatch(actions.rateRegistration(id, score,
-                () => null,
+                score => setScore(score),
                 errors => setBackendErrors(errors)));
 
         } else {
@@ -25,7 +27,45 @@ const RateRegistrationForm = ({id}) => {
         }
     }
 
-    return null;
+    return (
+        <div>
+            <Errors errors={backendErrors}
+                    onClose={() => setBackendErrors(null)}/>
+            <div className="card bg-light border-dark">
+                <h5 className="card-header">
+                    <FormattedMessage id="project.registrations.rate.title"/>
+                </h5>
+                <div className="card-body">
+                    <form ref={node => form = node}
+                          className="needs-validation" noValidate
+                          onSubmit={(e) => handleSubmit(e)}>
+                        <div className="form-group row">
+                            <label htmlFor="punctuation" className="col-md-3 col-form-label">
+                                <FormattedMessage id="project.global.fields.rating"/>
+                            </label>
+                            <div className="col-md-4">
+                                <input type="text" id="punctuation" className="form-control"
+                                       value={score}
+                                       onChange={e => setScore(e.target.value)}
+                                       autoFocus
+                                       required/>
+                                <div className="invalid-feedback">
+                                    <FormattedMessage id='project.global.validator.required'/>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="form-group row">
+                            <div className="offset-md-3 col-md-1">
+                                <button type="submit" className="btn btn-primary">
+                                    <FormattedMessage id="project.global.buttons.ratingButton"/>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default RateRegistrationForm;
